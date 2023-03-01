2420278bb3dc4d72aef7c13f1afed9cc52c52d96
BearHumanS <bye0651@naver.com>
2023-03-01 17:22:37 +0900

    Add files via upload

---
+++ app.js
@@ -0,0 +1,20 @@
+const start = document.querySelector("#start");
+const result = document.querySelector("#result");
+
+function startLotto() {
+  const lottoNumbers = [];
+  while (lottoNumbers.length < 6) {
+    const lottoNumber = Math.floor(Math.random() * 45) + 1;
+    if (!lottoNumbers.includes(lottoNumber)) {
+      lottoNumbers.push(lottoNumber);
+    }
+  }
+
+  lottoNumbers.sort(function (a, b) {
+    return a - b;
+  });
+
+  result.innerHTML = lottoNumbers;
+}
+
+start.addEventListener("click", startLotto);
